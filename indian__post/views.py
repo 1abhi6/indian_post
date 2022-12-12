cc1c8031f86fe2e3baf05c94d3dbe3cb1504988d
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-12-12 08:35:39 +0530

    Displaying the data

--- indian__post/views.py
+++ indian__post/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+import requests
 
 
 def home(request):
-    return render(request, "index.html")
+    pin_code = "401303"
+    url = "https://api.postalpincode.in/pincode/{}"
+    res = requests.get(url.format(pin_code)).json()
+    print(type(len(res[0]['PostOffice'])))
+    print(type(res[0]['PostOffice']))
+    lengthOfResponse = len(res[0]['PostOffice'])
+    res_data = {
+        'message': res[0]['Message'],
+        'name': []
+    }
+    result={
+        'res_data':res_data
+    }
+    for i in range(0, lengthOfResponse):
+        res_data['name'].append(res[0]['PostOffice'][i])
+    print(res_data)
+
+    return render(request, "index.html", result)
