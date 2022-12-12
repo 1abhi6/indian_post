bbf1847908495872f35ab3d98ad913da7f6ef1d3
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-12-12 09:16:54 +0530

    Data Table setup

--- indian__post/views.py
+++ indian__post/views.py
@@ -6,18 +6,23 @@ def home(request):
     pin_code = "401303"
     url = "https://api.postalpincode.in/pincode/{}"
     res = requests.get(url.format(pin_code)).json()
-    print(type(len(res[0]['PostOffice'])))
-    print(type(res[0]['PostOffice']))
-    lengthOfResponse = len(res[0]['PostOffice'])
+   
     res_data = {
         'message': res[0]['Message'],
-        'name': []
+        'name': [],
+        # 'lengthData':[]
     }
-    result={
-        'res_data':res_data
-    }
+    lengthOfResponse = len(res[0]['PostOffice'])
+
     for i in range(0, lengthOfResponse):
         res_data['name'].append(res[0]['PostOffice'][i])
-    print(res_data)
+        # res_data['lengthData'].append(i+1)
+    
+
+
+
+    result = {
+        'res_data': res_data
+    }
 
     return render(request, "index.html", result)
