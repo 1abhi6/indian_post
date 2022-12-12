258b1f667adadbedebcf130c2a1d5ae66049fe38
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2022-12-12 09:34:09 +0530

    pin input setup completed

--- indian__post/views.py
+++ indian__post/views.py
@@ -3,26 +3,36 @@ import requests
 
 
 def home(request):
-    pin_code = "401303"
-    url = "https://api.postalpincode.in/pincode/{}"
-    res = requests.get(url.format(pin_code)).json()
-   
-    res_data = {
-        'message': res[0]['Message'],
-        'name': [],
-        # 'lengthData':[]
-    }
-    lengthOfResponse = len(res[0]['PostOffice'])
+    result = {}
+    res_data = {}
+    if request.method == 'POST':
+        try:
 
-    for i in range(0, lengthOfResponse):
-        res_data['name'].append(res[0]['PostOffice'][i])
-        # res_data['lengthData'].append(i+1)
-    
+            pin_code = int(request.POST.get('pinInput'))
+            url = "https://api.postalpincode.in/pincode/{}"
+            res = requests.get(url.format(pin_code)).json()
 
+            res_data = {
+                'message': res[0]['Message'],
+                'name': [],
+                'status':True,
+                'pin_code':pin_code
+                # 'lengthData':[]
+            }
+            lengthOfResponse = len(res[0]['PostOffice'])
 
+            for i in range(0, lengthOfResponse):
+                res_data['name'].append(res[0]['PostOffice'][i])
+                # res_data['lengthData'].append(i+1)
+        
+        except:
+            res_data={
+                'message': "Pin code do not exist. Please check your PIN Code and try again"
+            }
 
     result = {
-        'res_data': res_data
+        'res_data': res_data,
+        
     }
 
     return render(request, "index.html", result)
